a2f914cc62e66ab9a5ba86c8194cc254f6a107d1
Aditya Goyal <43295337+adityagoyal222@users.noreply.github.com>
2020-10-20 18:49:42 +0545

    created User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib import auth
 
 # Create your models here.
+class User(auth.models.User, auth.models.PermissionsMixin):
+
+    def __str__(self):
+        return self.username
