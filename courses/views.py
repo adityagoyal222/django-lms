28cbe60ae68c970ffc9660f808a51a93873ae7a2
Aditya Goyal <43295337+adityagoyal222@users.noreply.github.com>
2020-10-21 19:31:34 +0545

    added a few views in courses

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
+from django.urls import reverse
+from django.views import generic
+from django.shortcuts import get_object_or_404
+from users.models import User
+from courses.models import Course, Enrollment
 
 # Create your views here.
+class CreateCourse(LoginRequiredMixin, generic.CreateView):
+    fields = ('course_name', 'course_description')
+    model = Course
+
+    def form_valid(self, form):
+        form.instance.teacher = self.request.user
+        return super(CreateCourse, self).form_valid(form)
