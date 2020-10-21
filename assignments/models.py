a38999b9f1f42d8770e17bdb0ffcb3ce593708ec
Aditya Goyal <43295337+adityagoyal222@users.noreply.github.com>
2020-10-21 18:47:04 +0545

    modified Assignment model

--- assignments/models.py
+++ assignments/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from users.models import User
+from courses.models import Course
 from django.urls import reverse
 
 # Create your models here.
@@ -8,13 +9,19 @@ class Assignment(models.Model):
     assignment_description = models.TextField()
     start_date = models.DateTimeField(auto_now=True)
     due_date = models.DateTimeField()
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return self.assignment_name
     # def def get_absolute_url(self):
     #     return reverse('', kwargs={'pk': self.pk})
 
 class SubmitAssignment(models.Model):
-    author = models.ForeignKey(User, related_name='assignment', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name='assignment', on_delete=models.SET_NULL)
     topic = models.CharField(max_length=200)
     description = models.TextField()
     assignment_file = models.FileField()
     submitted_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.topic
