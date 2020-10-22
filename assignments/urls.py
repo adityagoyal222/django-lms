26d738d1000cbf8d240e4ceff3df33d519b2344f
Aditya Goyal <43295337+adityagoyal222@users.noreply.github.com>
2020-10-22 21:29:49 +0545

    created detail, update and delete view for assignments

--- assignments/urls.py
+++ assignments/urls.py
@@ -5,4 +5,6 @@ app_name = 'assignments'
 urlpatterns = [
     url(r'^create/$', views.CreateAssignment.as_view(), name="create"),
     url(r'^detail/(?P<pk>[-\w]+)/$', views.AssignmentDetail.as_view(), name='detail'),
+    url(r'^update/(?P<pk>[-\w]+)/$', views.UpdateAssignment.as_view(), name='update'),
+    url(r'^delete/(?P<pk>[-\w]+)/$', views.DeleteAssignment.as_view(), name="delete"),
 ]
--- assignments/views.py
+++ assignments/views.py
@@ -11,21 +11,7 @@ from assignments.models import Assignment, SubmitAssignment
 from assignments.forms import GradeAssignmentForm, CreateAssignmentForm
 from courses.models import Course
 
-# Create your views here.
-# class CreateAssignment(LoginRequiredMixin, generic.CreateView):
-#     form_class = CreateAssignmentForm
-#     template_name = "assignments/create_assignment_form.html"
-
-#     def get_from_kwargs(self):
-#         # kwargs = super().get_from kwargs()
-#         # course = Course.objects.filter(teacher=self.request.user).order_by('course_name')
-#         # kwargs['course'] = course
-#         kwargs = super().get_form_kwargs()
-#         kwargs['user'] = self.request.user
-#         print(kwargs, 'hello')
-#         return kwargs
-
-    
+# Create your views here.    
 class CreateAssignment(LoginRequiredMixin, generic.CreateView):
     form_class = CreateAssignmentForm
     template_name = 'assignments/create_assignment_form.html'
@@ -38,3 +24,17 @@ class CreateAssignment(LoginRequiredMixin, generic.CreateView):
 
 class AssignmentDetail(generic.DetailView):
     model = Assignment
+
+class UpdateAssignment(LoginRequiredMixin, generic.UpdateView):
+    model = Assignment
+    form_class = CreateAssignmentForm
+    template_name = 'assignments/create_assignment_form.html'
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+class DeleteAssignment(LoginRequiredMixin, generic.DeleteView):
+    model = Assignment
+    success_url = reverse_lazy('courses:list')
