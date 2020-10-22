b319de97cd19c54dc944414ce4009baa2c580212
Aditya Goyal <43295337+adityagoyal222@users.noreply.github.com>
2020-10-22 17:10:22 +0545

    Created and updated list and detail templates.

--- courses/urls.py
+++ courses/urls.py
@@ -5,5 +5,8 @@ app_name = "courses"
 
 urlpatterns = [
     url(r'^new/$', views.CreateCourse.as_view(), name="create"),
-    url(r'^detail/(?P<pk>[-\w]+)/$', views.CourseDetail.as_view(), name='detail')
+    url(r'^detail/(?P<pk>[-\w]+)/$', views.CourseDetail.as_view(), name='detail'),
+    url(r'^all/', views.ListCourse.as_view(), name="list"),
+    url(r'^enroll/(?P<pk>[-\w]+)/$', views.EnrollCourse.as_view(), name='enroll'),
+    url(r'^unenroll/(?P<pk>[-\w]+)/$', views.UnenrollCourse.as_view(), name='unenroll'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import (LoginRequiredMixin,
                                         PermissionRequiredMixin)
 from django.urls import reverse
+from django.contrib import messages
 from django.views import generic
 from django.shortcuts import get_object_or_404
 from users.models import User
@@ -28,4 +29,37 @@ class CourseDetail(generic.DetailView):
 class ListCourse(generic.ListView):
     model = Course
 
-# class EnrollCourse(LoginRequiredMixin, generic.RedirectView):
+class EnrollCourse(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('courses:detail', kwargs={'pk':self.kwargs.get('pk')})
+    
+    def get(self, *args, **kwargs):
+        course = get_object_or_404(Course, pk=self.kwargs.get('pk'))
+
+        try:
+            Enrollment.objects.create(student=self.request.user, course=course)
+        except:
+            messages.warning(self.request, 'You are already enrolled in the course.')
+        else:
+            messages.success(self.request, 'You are now enrolled in the course.')
+        return super().get(self.request, *args, **kwargs)
+
+class UnenrollCourse(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('courses:detail', kwargs={'pk':self.kwargs.get('pk')})
+
+    def get(self, *args, **kwargs):
+
+        try:
+            enrollment = Enrollment.objects.filter(
+                student=self.request.user,
+                course__pk=self.kwargs.get('pk')
+            ).get()
+        except Enrollment.DoesNotExist:
+            messages.warning(self.request, 'You are not enrolled in this course.')
+        else:
+            enrollment.delete()
+            messages.success(self.request, 'You have unenrolled from the course.')
+        return super().get(self.request, *args, **kwargs)
