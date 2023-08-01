a553615376ce6020ebb215d94e323fcf337137bc
Mickmacha <kimmike1100@gmail.com>
2023-08-01 14:19:42 +0300

    updated git_ignore

--- editor/views.py
+++ editor/views.py
@@ -6,6 +6,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 def ide(request):
+    
     inputr = int(input("Enter your value: "))
     output = codex_api('val = ' + str(inputr) + ' + 5\nprint(val)', 'py', 5)
     # output = codex_api('val = int(input("Enter your value: ")) + 5\nprint(val)', 'py', 5)
