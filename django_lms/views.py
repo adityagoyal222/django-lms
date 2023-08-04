bac81bec5598311d081a92f735be3e5529f04de1
Peter Kyale <kyalepeter2000@gmail.com>
2023-08-04 15:42:58 +0300

    Updated the chatbot

--- django_lms/views.py
+++ django_lms/views.py
@@ -6,6 +6,10 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import requests
 import openai
+from django.contrib.sessions.backends.db import SessionStore
+from django.http import HttpResponseServerError
+import os
+from dotenv import load_dotenv
 
 class UserProfile(LoginRequiredMixin, generic.ListView):
     model = Course
@@ -18,27 +22,48 @@ def index(request):
 
 # @csrf_exempt 
 def send_chatbot_response(request):
-    api_key = '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'
-    openai.api_key = api_key
+    # Load environment variables from .env file
+    load_dotenv()
+    openai.api_key = os.getenv('OPENAI_API_KEY')
+    # openai.api_key = api_key
+
     if request.method == 'POST':
         sender = request.POST.get("chattext")
         print(sender)
-        # sender = "what is the fifa 23 in 5 words"
-        messages = [{"role": "system", "content": "You are a knowledgable blockchain assistant."}]
-        messages.append({"role": "user", "content": sender})
-        # sender = "Who is elon musk"
-        while sender != "":
-            user_msg = sender
-            messages.append({"role": "user", "content": user_msg})
-            response  = openai.ChatCompletion.create(model='gpt-3.5-turbo',  messages=messages)
-            bot_reply = response['choices'][0]['message']['content']
-            print(bot_reply)
-            messages.append({"role": "system", "content": bot_reply})
-            sender = bot_reply
 
-            context = {
-                'response': messages
-            }
+        session_id = request.META.get('HTTP_X_SESSION_ID', None)
+
+        if not session_id:
+            # If the session ID is not provided, create a new session
+            session = SessionStore()
+            session.create()
+            session_id = session.session_key
+
+         # Get or create the session with the provided session ID
+        session = SessionStore(session_key=session_id)
+
+        
+        # Retrieve the chat history from the session
+        messages = session.get("chat_history", [])
+
+        if not messages:
+            messages.append({"role": "system", "content": "You are a knowledgeable blockchain assistant."})
+
+        # Append the new user message to the chat history
+        messages.append({"role": "user", "content": sender})
+
+        response = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=messages)
+        bot_reply = response['choices'][0]['message']['content']
+        print(bot_reply)
+
+        messages.append({"role": "system", "content": bot_reply})
+         # Save the updated chat history back to the session
+        session["chat_history"] = messages
+        session.save()
+
+        context = {
+            'response': messages
+        }
 
         return render(request, 'base.html', context)
     else:
