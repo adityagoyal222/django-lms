ef65a5659a28258b2f9bb1d9f8a92bda65693535
Peter Kyale <kyalepeter2000@gmail.com>
2023-11-24 16:15:59 +0300

    configured the quiz session remaining attempts for the quizes

--- assignments/views.py
+++ assignments/views.py
@@ -28,6 +28,9 @@ from .models import QuizSubmission, UserProfile
 from django.urls import reverse
 from django.contrib.sessions.models import Session
 import requests
+from datetime import datetime, timedelta
+from django.contrib.sessions.models import Session
+from django.utils import timezone
 
 # Create your views here.    
 class CreateAssignment(LoginRequiredMixin, generic.CreateView):
@@ -38,21 +41,6 @@ class CreateAssignment(LoginRequiredMixin, generic.CreateView):
         kwargs = super().get_form_kwargs()
         kwargs['user'] = self.request.user
         return kwargs
-
-# class CreateQuizView(LoginRequiredMixin, generic.CreateView):
-#     model = Quiz
-#     form_class = QuizForm
-#     template_name = 'assignments/create_quiz.html'
-
-#     def get_form_kwargs(self):
-#         kwargs = super().get_form_kwargs()
-#         kwargs['user']= self.request.user
-#         return kwargs
-
-#     def form_valid(self, form):
-#         user_object = get_object_or_404(User, username=self.request.user.username)
-#         form.instance.teacher = user_object
-#         return super().form_valid(form)
 
 ChoiceFormSet = modelformset_factory(Choice, fields=('text', 'is_correct'), extra=4, max_num=4)
 
@@ -140,25 +128,6 @@ def create_quiz(request):
         quiz_form = QuizForm(user=request.user)
     return render(request, 'assignments/create_quiz.html', {'quiz_form': quiz_form})
 
-# def create_question(request, quiz_id):
-#     quiz = Quiz.objects.get(id=quiz_id)
-#     if request.method == 'POST':
-#         question_form = QuestionForm(request.POST)
-#         choice_formset = ChoiceFormSet(request.POST)
-#         if question_form.is_valid() and choice_formset.is_valid():
-#             question = question_form.save(commit=False)
-#             question.quiz = quiz
-#             question.save()
-#             choice_formset.instance = question
-#             choice_formset.save()
-#             return redirect('create_question', quiz_id=quiz_id)
-#     else:
-#         question_form = QuestionForm()
-#         choice_formset = ChoiceFormSet()
-#     return render(request, 'assignments/create_question.html', {'question_form': question_form, 'choice_formset': choice_formset})
-    
-    
-
 class UpdateAssignment(LoginRequiredMixin, generic.UpdateView):
     model = Assignment
     form_class = CreateAssignmentForm
@@ -192,39 +161,6 @@ class SubmitAssignmentView(LoginRequiredMixin, generic.CreateView):
         kwargs['assignment_id'] = self.request.session.get('assignment')
         kwargs['user'] = self.request.user
         return kwargs
-    
-# class SubmitQuizView(LoginRequiredMixin, generic.CreateView):
-#     form_class = SubmitQuizForm
-#     template_name = 'assignments/submitquiz_form.html'
-#     select_related = ('student', 'quiz')
-    
-#     def get_form_kwargs(self):
-#         kwargs = super().get_form_kwargs()
-#         kwargs['user'] = self.request.user
-#         kwargs['quiz'] = Quiz.objects.get(id=self.kwargs['quiz_id'])
-#         return kwargs
-    
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['quiz'] = Quiz.objects.get(id=self.kwargs['quiz_id'])
-#         return context
-    
-#     def form_valid(self, form):
-#         quiz = Quiz.objects.get(id=self.kwargs['quiz_id'])
-#         self.object = form.save(commit=False)
-#         self.object.quiz = quiz
-#         self.object.student = self.request.user
-        
-#         score = 0
-#         for question in quiz.question_set.all():
-#             selected_choices = form.cleaned_data[f'question_{question.id}']
-#             correct_choices = question.choice_set.filter(is_correct=True)
-#             if set(selected_choices) == set(correct_choices):
-#                 score += 1
-#         self.object.score = score
-#         self.object.save()
-        
-#         return redirect('quiz_results', submission_id=self.object.id)
     
 class QuizResultsView(LoginRequiredMixin, generic.TemplateView):
     model = QuizSubmission
@@ -275,16 +211,9 @@ class QuizAnswerView(LoginRequiredMixin, FormView):
         kwargs['quiz_id'] = self.kwargs['quiz_id']
         return kwargs
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["quiz_id"] = self.kwargs.get('quiz_id')
-        context["course_id"] = self.get_course_id()
-        
-        return context
-    
-
     def get_quiz(self):
         return get_object_or_404(Quiz, pk=self.kwargs['quiz_id'])
+    
     def get_course_id(self):
         quiz = self.get_quiz()
         chapter = quiz.chapter  
@@ -304,7 +233,51 @@ class QuizAnswerView(LoginRequiredMixin, FormView):
 
     def form_valid(self, form):
         quiz = self.get_quiz()
+        session_key = self.request.session.session_key
+        quiz_session_key = f"quiz_attempts_{self.request.user.id}_{quiz.id}"
+        if not session_key:
+            self.request.session.save()
+            session_key = self.request.session.session_key
+
+
+        if quiz_session_key not in self.request.session:
+            self.request.session[quiz_session_key] = 3  # Initialize attempts for the quiz
+
+
+        # Check if the user has a quiz attempt session variable
+        if 'quiz_attempts' not in self.request.session:
+            self.request.session['quiz_attempts'] = 3
+            self.request.session['quiz_last_attempt'] = int(timezone.now().timestamp())
+            print("Stored last attempt time:", self.request.session['quiz_last_attempt'])
+
+
+
+        if 'quiz_attempts' not in self.request.session or 'quiz_last_attempt' not in self.request.session:
+            # Handle session expiration or start a new session
+            messages.warning(self.request, "Your session has expired. Please log in again.")
+            return super().form_invalid(form)
+        else:
+            last_attempt_time = timezone.datetime.fromtimestamp(self.request.session['quiz_last_attempt'])
+            remaining_time_seconds = max(0, (last_attempt_time.astimezone(timezone.get_current_timezone()) + timedelta(hours=24) - timezone.now()).total_seconds())
+        # context["remaining_time_seconds"] = remaining_time_seconds
+
+
+        # Check if the user has attempts remaining
+        remaining_attempts = self.request.session.get(quiz_session_key, 0)
+        # When retrieving the last_attempt_time
+        last_attempt_time = timezone.datetime.fromtimestamp(self.request.session['quiz_last_attempt'])
+        print("Retrieved last attempt time:", last_attempt_time)
+
+        cooldown_time = last_attempt_time + timedelta(hours=24)
+
+        if remaining_attempts <= 0 and timezone.now() < cooldown_time.replace(tzinfo=timezone.get_current_timezone()):
+            messages.warning(self.request, "You have used all your attempts for this quiz. Please try again after 24 hours.")
+            return super().form_invalid(form)
+
+        # Process the quiz submission
         score = self.calculate_score(form)
+
+
         
         try:
             # Try to create a new QuizSubmission instance
@@ -320,39 +293,51 @@ class QuizAnswerView(LoginRequiredMixin, FormView):
             submission.score = score
             submission.save()
 
+         # Update the user's quiz attempts in the session
+        self.request.session[quiz_session_key] -= 1
+        self.request.session['quiz_last_attempt'] = timezone.now().timestamp()
+
+
         # Retrieve the UserProfile or create a new one if it doesn't exist
         user_profile, created = UserProfile.objects.get_or_create(user=self.request.user)
 
+        # Calculate the percentage score
+        total_questions = quiz.question_set.count()
+        percentage_score = round((score / total_questions) * 100)
+
         # Check if the score is >= 75%
         total_questions = quiz.question_set.count()
-        if (score / total_questions) * 100 >= 75:
+        if percentage_score >= 75:
             # If score is >= 75%, add the quiz ID to completed quizzes
-            # messages.success(self.request, f"Your score is {round((score / total_questions) * 100)}%. You has passed this quiz.")
             if quiz.id not in user_profile.completed_quizzes.all():
                 # If not, add the quiz ID to completed quizzes
                 quiz_id = self.kwargs.get('quiz_id')
                 user_profile.completed_quizzes.add(quiz.id)
-                messages.success(self.request, f"Your score is {round((score / total_questions) * 100)}%. You have passed this quiz.")
+                messages.success(self.request, f"Your score is {percentage_score}%. You have passed this quiz.")
                 print("Completed Quizzeeees:", user_profile.completed_quizzes.all())
-                # get the quiz_ids in the completed_quizes field
-
+            # get the quiz_ids in the completed_quizes field
         else:
-            messages.warning(self.request, f"Your score is {round((score / total_questions) * 100)}%. You has failed this quiz.")
+            messages.warning(self.request, f"Your score is {percentage_score}%. You have failed this quiz.")
+
         
-        # Check if user has 3 or more attempts
-        remaining_attempts = 3 - quiz.quizsubmission_set.filter(student=self.request.user).count()
-        
-        if remaining_attempts <= 0:
-            messages.warning(self.request, "You have used all your attempts for this quiz.")
+        # Check if user has attempts remaining
+        remaining_attempts = self.request.session.get(quiz_session_key, 0)
+
+        if remaining_attempts <= 0 and timezone.now() < cooldown_time.replace(tzinfo=timezone.get_current_timezone()):
+            messages.warning(self.request, "You have used all your attempts for this quiz. Please try again after 24 hours.")
         else:
             messages.info(self.request, f"You have {remaining_attempts} attempt(s) remaining for this quiz.")
 
+        
         self.kwargs['submission_id'] = submission.id
         # remaining attempts context
-        print("Remaining attempts:", remaining_attempts)
+        print("Remaining attempts1:", remaining_attempts)
         # Include quiz_id in the context when calling get_context_data
         # return super().form_valid(form, quiz_id=self.kwargs['quiz_id'])
         quiz_id = self.kwargs.get('quiz_id')
+        print("Percentage score:", percentage_score)
+        percentage_score = round((score / total_questions) * 100)
+        print("Percentage score:", percentage_score)
         print("Quiz ID:", quiz_id)
         print("course ID:", self.get_course_id())
         return super().form_valid(form)
@@ -371,15 +356,13 @@ class QuizAnswerView(LoginRequiredMixin, FormView):
         # Access quiz_id directly from self.kwargs
         quiz_id = self.kwargs.get('quiz_id')
         context["quiz_id"] = quiz_id
+        context["course_id"] = self.get_course_id()
+        # Check if 'quiz_last_attempt' is present in the session
+        if 'quiz_last_attempt' in self.request.session:
+            last_attempt_time = timezone.datetime.fromtimestamp(self.request.session['quiz_last_attempt'])
+            remaining_time_seconds = max(0, (last_attempt_time.astimezone(timezone.get_current_timezone()) + timedelta(hours=24) - timezone.now()).total_seconds())
+            context["remaining_time_seconds"] = remaining_time_seconds
         return context
-
-    
-   
-    
-
-
-
-
 
 
 @login_required
